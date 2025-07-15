9a5dca8cef8ad7510ce5b8e1e7ceea1b13718592
Viktor Seniukov <spinnekopser@gmail.com>
2025-07-15 20:33:14 +0200

    init nuxt and base project configuration

---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+import {fileURLToPath} from 'node:url';
+
+export default defineNuxtConfig({
+    compatibilityDate: '2025-05-15',
+    devtools: { enabled: true },
+    modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxtjs/i18n'],
+
+    i18n: {
+        langDir: '../src/plugins/i18n/locales',
+        defaultLocale: 'en',
+        locales: [
+            {code: 'en', name: 'English', file: 'en.json'},
+            {code: 'ru', name: 'Russian', file: 'ru.json'},
+            {code: 'ro', name: 'Romanian', file: 'ro.json'},
+        ]
+    },
+
+    css: ['~/assets/css/main.css'],
+    srcDir: 'src',
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
+    alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+        '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+        '@routes': fileURLToPath(new URL('./src/routes', import.meta.url)),
+        '@constants': fileURLToPath(new URL('./src/constants', import.meta.url)),
+        '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
+        '@api': fileURLToPath(new URL('./src/api', import.meta.url)),
+    }
+});
---
+++ src/features/useInputUserName.ts
@@ -0,0 +1,3 @@
+export const useInputUserName = (): null | string => {
+    return 'John';
+};
