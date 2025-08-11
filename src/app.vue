0d5309a11b0f712fc40f1d8b9d06151c36fb66c5
Viktor Seniukov <spinnekopser@gmail.com>
2025-08-11 19:35:39 +0200

    added custom route

--- src/app.vue
+++ src/app.vue
@@ -1,22 +1,5 @@
-<script setup lang="ts">
-    import {useInputUserName} from '@features/useInputUserName';
-
-    const _x = '123';
-    const name = ref<string>('');
-
-    onMounted(() => {
-        name.value = useInputUserName();
-    });
-</script>
-
 <template>
-    <h1>{{ $t('welcome') }}</h1>
-    <h2
-        class="text-3xl font-bold underline"
-        title="123"
-    >
-        Hello world!
-
-        <pre>Your name: {{ name }}</pre>
-    </h2>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
