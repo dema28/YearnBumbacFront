fcf99a918cdb8fa9a9ec8d80a961830b9e3d36c3
Viktor Seniukov <100833471+ViktorSeniukov@users.noreply.github.com>
2025-08-11 19:41:19 +0200

    added custom route (#3)

---
+++ src/components/header/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="container mx-auto px-6 py-4 flex items-center justify-between">
+        <NuxtLink class="font-['Pacifico'] text-3xl text-primary">
+            Bumbac
+        </NuxtLink>
+    </header>
+</template>
---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import Header from '@components/header/Header.vue';
+</script>
+
+<template>
+    <div>
+        <Header />
+
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
